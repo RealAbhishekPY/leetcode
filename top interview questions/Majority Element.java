f002ca32876b4993e9548fe5248a525704f863aa
Abhishek Ambulkar <abhishekambulkar2017@gmail.com>
2025-03-11 14:41:35 +0530

    Create Majority Element.java

---
+++ top interview questions/Majority Element.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int majorityElement(int[] num) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int n : num) {
+            if (count == 0) {
+                candidate = n;
+            }
+            count += (n == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
