a9357d4cba6686613340c6515674627f4cb01b40
Abhishek Ambulkar <abhishekambulkar2017@gmail.com>
2025-03-11 14:15:18 +0530

    Create removeElement.java

---
+++ top interview questions/removeElement.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if(A.length==0) return 0;
+        int i=0;
+        int j=0;
+        while(i<A.length){
+            if(A[i]==elem){
+                i++;
+            }
+            else {
+                A[j]=A[i];
+                j++;
+                i++;
+            }
+        }
+        return j++;
+    }
+}
