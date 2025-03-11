41e6c37aba54a1ac2751a43f581b6f3f48beeca2
Abhishek Ambulkar <abhishekambulkar2017@gmail.com>
2025-03-11 14:40:52 +0530

    Create Remove Duplicates from Sorted Array II.java

---
+++ top interview questions/Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+
+        int idx = 1; // Start from the second element
+        int count = 1; // Count occurrences of the current element
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] == A[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count <= 2) {
+                A[idx++] = A[i];
+            }
+        }
+
+        return idx;
+    }
+}
